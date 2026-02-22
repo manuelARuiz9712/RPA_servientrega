b5295e41472e243beaa11dbef34f524f124a802b
manuelARuiz9712 <35358380+manuelARuiz9712@users.noreply.github.com>
2026-02-21 20:08:17 -0500

    add logs

--- app.controller.js
+++ app.controller.js
@@ -7,7 +7,8 @@ export class AppController {
     async ObtnerDatosGuia(req, res) {
         const { guia_id } = req.params;
         console.log("resolve");
-        console.log(req.NavigatorHelper.navigatorInstance)
+        console.log("nav helper",req.NavigatorHelper);
+        console.log("nav instance",req.NavigatorHelper.navigatorInstance)
         const result = await req.NavigatorHelper.RunRequest(
             getGuia,
             [req.NavigatorHelper.navigatorInstance, guia_id]
